023e6cb805a04fc4cada8e047d1d09bac7b0cb36
zunggzing <hello.trungvinh@gmail.com>
2021-07-24 11:45:31 +0700

    Connect to redux

---
+++ src/reducers/dist/index.dev.js
@@ -0,0 +1,12 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+var _redux = require("redux");
+
+var myReducers = (0, _redux.combineReducers)({});
+var _default = myReducers;
+exports["default"] = _default;
